db226321d59693d377d728895fd9a1beee78e7ab
Gervasio Machado <gervasio.machadozorrilla@gmail.com>
2022-06-20 16:51:01 -0300

    subiendo producto

---
+++ producto.py
@@ -0,0 +1,8 @@
+#producto de dos parametros
+
+def producto():
+  a = int(input('Ingrese el primer parametro: '))
+  b = float(input('Ingrese el segundo parametro: '))
+  resultado = a * b
+  return resultado
+print(producto())  
